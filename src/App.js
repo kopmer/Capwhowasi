8e36337ae606b4a3e22053ddbac1a32854a61c69
kopmer <cici12358@naver.com>
2023-09-29 14:22:14 +0900

    v1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import KcfApp from './components/KcfApp/KcfApp';
+function App() {
+  return (
+    <div className="App">
+      <KcfApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/KcfApp/funclist.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Button(props) {
+    const { label, styleClass, onClick, disabled} = props;
+    return (
+        <button className={styleClass} onClick={onClick} disabled = {disabled}>{label}</button>
+    )
+}
+
+
+
+export {Button};
